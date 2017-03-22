02549aa6027baba54d8383d7f9f716a238533b2a
jecons@live.cn <jecons@live.cn>
2017-03-22 13:22:25 +0800

    提交代码分支管理

---
+++ TestModuleDevelopment/app/src/main/java/com/help/reward/shop/ComponentApplication.java
@@ -0,0 +1,12 @@
+package com.help.reward.shop;
+
+/**
+ * Created by richsjeson on 17/3/22.
+ * 组件入口类，管理application和context，以及activity的生命
+ */
+
+public class ComponentApplication {
+
+
+
+}
---
+++ TestModuleDevelopment/app/src/main/java/com/help/reward/shop/LauncherModel.java
@@ -0,0 +1,9 @@
+package com.help.reward.shop;
+
+/**
+ * Created by richsjeson on 17/3/22.
+ * 模块装载，用于装载所有模块
+ */
+
+public class LauncherModel {
+}
