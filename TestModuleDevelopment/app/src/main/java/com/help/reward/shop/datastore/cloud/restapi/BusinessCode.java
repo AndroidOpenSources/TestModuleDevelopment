5986b8ac7f43c91b58b0d2bf155f04fecc5b88b0
leaderbean <634051075@qq.com>
2017-03-25 20:49:58 +0800

    提交模块代码

--- TestModuleDevelopment/app/src/main/java/com/help/reward/shop/datastore/cloud/restapi/BusinessCode.java
+++ TestModuleDevelopment/app/src/main/java/com/help/reward/shop/datastore/cloud/restapi/BusinessCode.java
@@ -6,8 +6,60 @@ package com.help.reward.shop.datastore.cloud.restapi;
 
 public class BusinessCode {
 
-    int code;
-    String msg;
-    String data;
+    private int code;
+    private String msg;
+    private String data;
+    private boolean hashmore;
+    private int page_total;
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public boolean isHashmore() {
+        return hashmore;
+    }
+
+    public void setHashmore(boolean hashmore) {
+        this.hashmore = hashmore;
+    }
+
+    public int getPage_total() {
+        return page_total;
+    }
+
+    public void setPage_total(int page_total) {
+        this.page_total = page_total;
+    }
+
+    @Override
+    public String toString() {
+        return "BusinessCode{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data='" + data + '\'' +
+                ", hashmore=" + hashmore +
+                ", page_total=" + page_total +
+                '}';
+    }
 }
--- TestModuleDevelopment/app/src/main/java/com/help/reward/shop/datastore/cloud/restapi/RequestShopRestApi.java
+++ TestModuleDevelopment/app/src/main/java/com/help/reward/shop/datastore/cloud/restapi/RequestShopRestApi.java
@@ -10,9 +10,23 @@ import retrofit2.http.Query;
 
 public interface RequestShopRestApi {
 
+    //商品详情
     @GET("mobile/index.php?act=goods&op=goods_detail")
     Call<String> getShopInfo(@Query("goods_id") Integer id);
+
     @GET("mobile/index.php?act=goods&op=goods_body_info")
     Call<String> getShopInstroduce(@Query("goods_id") Integer id);
+
+    //全部商品-搜索
+    @GET("/mobile/index.php?act=goods&op=goods_list")
+    Call<String> getShopList();
+
+    //获取历史搜索和热门搜索
+    @GET("/mobile/index.php?act=index&op=search_key_list")
+    Call<String> getSearchKeyList();
+
+    //类别列表
+    @GET("/mobile/index.php?act=goods_class&op=index")
+    Call<String> getClassList();
 
 }
